6e229a344d29d89b7761f004c01156a37bd60d22
WilliamMcCann <nwilliammccann@gmail.com>
2016-03-05 16:00:55 -0800

    adding zero

--- 0-print/main.go
+++ 0-print/main.go
@@ -1,1 +1,15 @@
-fkdlsa;hfl;shjlghdlasjfkldsajl
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	fmt.Println("Hello, we are Holberton School")
+	fmt.Printf("the date is %v\n", t)
+	fmt.Printf("the year is %v\n", (t.Year()))
+	fmt.Printf("the month is %v\n", (t.Month()))
+	fmt.Printf("the day is %v\n", (t.Day()))
+}
